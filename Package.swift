5d6073dd0c849b154f3e31e9837d09d683f2be7d
Taylor Holliday <wtholliday@gmail.com>
2024-10-10 21:48:55 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "manifold",
+    platforms: [
+        .macOS(.v11), .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "manifold",
+            targets: ["manifold"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/audulus/glm", branch: "spm"),
+        .package(url: "https://github.com/audulus/Clipper2", branch: "spm"),
+    ],
+    targets: [
+        .target(
+            name: "manifold",
+            dependencies: ["glm", "Clipper2"],
+            path: ".",
+            exclude: ["src/meshIO", "src/CMakeLists.txt"],
+            sources: ["src"],
+            publicHeadersPath: "include",
+            cxxSettings: [
+                .define("MANIFOLD_PAR", to: "-1"),
+                .unsafeFlags(["-Wno-shorten-64-to-32"])
+            ]
+        )
+    ],
+    cxxLanguageStandard: .cxx20
+)
