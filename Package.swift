bf561e9914e66504b0397d7b132567b76d21cd82
Taylor Holliday <wtholliday@gmail.com>
2024-10-25 23:00:00 -0700

    remove glm

--- Package.swift
+++ Package.swift
@@ -12,13 +12,12 @@ let package = Package(
             targets: ["manifold"])
     ],
     dependencies: [
-        .package(url: "https://github.com/audulus/glm", branch: "spm"),
         .package(url: "https://github.com/audulus/Clipper2", branch: "spm"),
     ],
     targets: [
         .target(
             name: "manifold",
-            dependencies: ["glm", "Clipper2"],
+            dependencies: ["Clipper2"],
             path: ".",
             exclude: ["src/meshIO", "src/CMakeLists.txt"],
             sources: ["src"],
